a39df76eefb8e15967cd52fabc9e6e9218a38820
norito-HAYASHI <84329571+piyorin-hub@users.noreply.github.com>
2026-03-09 22:07:15 +0900

    ProviderScope追加

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'pages/task_list_page.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(ProviderScope(child: const MyApp()));
 }
 
 class MyApp extends StatelessWidget {
